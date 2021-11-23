f15c706fdeba905956ab7658240c8258962a56fc
Mahfudin98 <mahfudinnawawi@gmail.com>
2021-11-23 11:11:02 +0700

    webpack add browsersync

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,27 @@ const mix = require("laravel-mix");
 
 mix.js("resources/js/app.js", "public/js")
     .vue()
-    .sass("resources/sass/app.scss", "public/css");
+    .sass("resources/sass/app.scss", "public/css")
+    .version()
+    .browserSync({
+        // host: '192.168.10.10',
+        // proxy: 'mywebsite.test',
+        open: false,
+        injectChanges: true,
+        logSnippet: true,
+        watchOptions: {
+            usePolling: true,
+            interval: 500,
+            poll: true,
+            ignored: /node_modules/
+        },
+        files: [
+            'app/**/*.php',
+            'resources/views/**/*.php',
+            'resources/js/app.js',
+            'resources/js/components/*.vue',
+            'packages/mixdinternet/frontend/src/**/*.php',
+            'public/js/**/*.js',
+            'public/css/**/*.css'
+        ]
+    });
